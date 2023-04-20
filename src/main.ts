14dc5fc54e73a5c57926fe55f36e29023bc47533
kimkh05 <nestjs01@naver.com>
2023-04-20 21:42:48 +0900

    fix: change function name

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const create = async () => {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-}
-bootstrap();
+};
+create();
