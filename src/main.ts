173ea1daab0263e885f2a1a0061269ce3266b9ed
kimkh05 <nestjs01@naver.com>
2023-04-27 10:40:41 +0900

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from 'utils/swagger';
 
 const create = async () => {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(3000);
 };
 create();
