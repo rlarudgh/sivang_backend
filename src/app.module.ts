94e9465a37fc9a9eca855d5d33ca61fdda0bfe7e
kimkh05 <nestjs01@naver.com>
2023-04-27 10:40:59 +0900

    feat: connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: parseInt('3306', 10),
+      username: 'root',
+      password: '1234',
+      database: 'sivang',
+      synchronize: 'true',
+      logging: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    JwtModule.register({
+      secret: 'sivang',
+      signOptions: { expiresIn: '60s' },
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
