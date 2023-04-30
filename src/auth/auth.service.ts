848d32f6184ee812a80e833558628d71eba42876
kimkh05 <nestjs01@naver.com>
2023-04-30 23:01:24 +0900

    fix: change error code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import { sign } from 'jsonwebtoken';
@@ -7,16 +8,22 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(User) private usersRepository: Repository<User>) {}
+  constructor(@InjectRepository(User) private usersRepository: Repository<User>, private jwtService: JwtService) {}
 
   async register(email: string, name: string, password: string, passwordConfirmation: string): Promise<User> {
     const existingUser: User = await this.usersRepository.findOne({ email });
+    const regex: RegExp = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+
+    if (!regex.test(email)) {
+      throw new BadRequestException('이메일 형식이 올바르지 않습니다.');
+    }
+
     if (existingUser) {
-      throw new Error('이메일이 이미 있습니다.');
+      throw new ConflictException('이메일이 이미 있습니다.');
     }
 
     if (password !== passwordConfirmation) {
-      throw new Error('비밀번호가 틀렸습니다.');
+      throw new BadRequestException('비밀번호가 틀렸습니다.');
     }
 
     const hashedPassword: string = await bcrypt.hash(password, 10);
