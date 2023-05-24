73c04bb193bc4dd33c245de0ac516c385aed206c
kimkh05 <nestjs01@naver.com>
2023-05-24 16:21:11 +0900

    feat: verifyToken

--- src/helper/jwt.ts
+++ src/helper/jwt.ts
@@ -6,16 +6,32 @@ interface JWTPayload {
 }
 
 export class JWTHelper {
-  public generateToken = ({ userId }: JWTPayload) => {
+  private secret: string;
+
+  constructor() {
+    this.secret = config.jwt.secret;
+  }
+
+  public generateToken = ({ userId }: JWTPayload): string => {
     return jwt.sign({ userId }, config.jwt.secret, { expiresIn: config.jwt.expiresIn });
   };
 
-  public generateAccessToken = ({ userId }: JWTPayload) => {
+  public generateAccessToken = ({ userId }: JWTPayload): string => {
     return jwt.sign({ userId }, config.jwt.secret, { subject: 'ACCESS_TOKEN' });
   };
 
-  public generateJWTToken = ({ userId }: JWTPayload) => {
+  public generateJWTToken = ({ userId }: JWTPayload): { access_token: string } => {
     const access_token = this.generateAccessToken({ userId });
     return { access_token };
+  };
+
+  public verifyJwtToken = async (token: string) => {
+    const decodedToken = await jwt.verify(token, this.secret);
+    return decodedToken;
+  };
+
+  public decodeAccessToken = async (token: string): Promise<JWTPayload> => {
+    const decodedAccessToken: JWTPayload = (await this.verifyJwtToken(token)) as JWTPayload;
+    return decodedAccessToken;
   };
 }
