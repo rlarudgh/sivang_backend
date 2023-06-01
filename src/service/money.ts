7c8c2ca3f514b00e99686abfe31b2d87f60656eb
kimkh05 <nestjs01@naver.com>
2023-06-01 19:58:41 +0900

    fix: money service

--- src/service/money.ts
+++ src/service/money.ts
@@ -1,38 +1,64 @@
 import { MoneyRepository } from '../repository/money';
-import { ProfileRepository } from '../repository/profile';
 import { PostMoneyType } from '../interface/money/postMoney';
+import { ErrorResponse } from '../utils/error-response';
+import { error } from '../constants/error';
+import { UpdateMoneyPostType } from '../interface/money/updateMoney';
 
 export class MoneyService {
   private moneyRepository: MoneyRepository;
-  private profileRepository: ProfileRepository;
 
   constructor() {
     this.moneyRepository = new MoneyRepository();
-    this.profileRepository = new ProfileRepository();
   }
 
   public createMoneyPost = async ({ userId, moneyInfo }: { userId: number; moneyInfo: PostMoneyType }) => {
     try {
-      const userProfile = await this.profileRepository.findByUserId(userId);
-
       await this.moneyRepository.createMoneyPost({
         userId,
         moneyInfo,
       });
     } catch (error: unknown) {
       console.error('가계부 작성 실패', error);
-      throw new Error('가계부 작성 실패');
+      throw new Error('Failed to create money entry');
     }
   };
 
   public getMoneyPostList = async (userId: number) => {
     try {
       const moneyPostList = await this.moneyRepository.getMoneyPostList(userId);
-
       return moneyPostList;
     } catch (error: unknown) {
       console.error(error);
       throw new Error('가계부 조회 실패');
     }
   };
+
+  public findMoneyPost = async (moneyId: number, userId: number) => {
+    try {
+      const moneyPost = await this.moneyRepository.findMoneyPost(moneyId, userId);
+
+      if (!moneyPost) {
+        throw new ErrorResponse(error.userError.notFound);
+      }
+
+      return moneyPost;
+    } catch (err: unknown) {
+      console.error(err);
+      throw new Error('가계부 아이템 조회 실패');
+    }
+  };
+
+  public updateMoneyPost = async({ userId, moneyId, moneyInfo }: UpdateMoneyPostType) => {
+    try{
+      const moneyPost = await this.moneyRepository.findMoneyPost(moneyId, userId);
+
+      if(!moneyPost){
+        throw new ErrorResponse(error.userError.notFound);
+      }
+
+      await this.moneyRepository.updateMoneyPost({ moneyId, moneyInfo });
+    }catch(err: unknown){
+      console.error(err);
+    }
+  }
 }
